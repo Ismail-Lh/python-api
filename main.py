7e10f55639cfb495967902d248a8904946684ca9
Ismail-Lh <ismailsaad03@gmail.com>
2024-05-25 19:33:35 +0100

    add schema validation for the post creation using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: float | None = None
 
 
 @app.get("/")
@@ -15,5 +23,7 @@ def get_all_posts():
 
 
 @app.post("/posts")
-def create_post(body: dict = Body(...)):
-    return {"message": f"Post with title {body["title"]} was created successfully!"}
+def create_post(post: Post):
+    message = f"Post with title {post.title} was created successfully!"
+
+    return {"data": post.model_dump(), "message": message}
