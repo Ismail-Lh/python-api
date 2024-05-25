5734e5e4f23d3e7ba8de4fc77aafe62e1c1c2db9
Ismail-Lh <ismailsaad03@gmail.com>
2024-05-25 18:52:43 +0100

    adding a POST HTTP request route

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World from another file!"}
+    return {"message": "Hello World from another file!!!!"}
+
+
+@app.get("/posts")
+def get_all_posts():
+    return {"posts": ["post1", "post2", "post3"]}
+
+
+@app.post("/posts")
+def create_post(body: dict = Body(...)):
+    return {"message": f"Post with title {body["title"]} was created successfully!"}
